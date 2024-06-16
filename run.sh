faa401d4a9907f6fa6cfd64453e16b8c93bd039e
JunZhang <119716645+LasRuinasCirculares@users.noreply.github.com>
2024-06-16 14:59:04 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=3,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1,2,3
 export RAY_memory_monitor_refresh_ms=0
 
 models=('/opt/data/private/szc/llm/Llama-2-7b-hf')
-data_path="/opt/data/private/szc/vllm_inference/co-temporal/equal.json"
-mode='few_shot'
-output_path="/opt/data/private/szc/vllm_inference/co-temporal/generate_result"
-result_path='/opt/data/private/szc/vllm_inference/co-temporal/co-temproal-datasets/evaluate_result'
+data_paths=("/opt/data/private/szc/co-temproal-datasets/overlap.json" "/opt/data/private/szc/co-temproal-datasets/equal.json" "/opt/data/private/szc/co-temproal-datasets/mix.json" "/opt/data/private/szc/co-temproal-datasets/during.json")
+mode='default'
+output_path="/opt/data/private/szc/co-temporal/generate_result"
+result_path='/opt/data/private/szc/co-temporal/evaluate_result'
 for model_name in "${models[@]}"
 do
-  python inference.py "$model_name" "$data_path" "$mode" "$output_path"  "$result_path"
+  for data_path in "${data_paths[@]}"
+  do
+    python inference.py "$model_name" "$data_path" "$mode" "$output_path" "$result_path"
+  done
 done
